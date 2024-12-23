3a37be18d6767bd440ff9e1c842895c53043d231
Fernando Esquirio Torres <esquirio@gmail.com>
2024-12-23 10:28:00 +0100

    fix: adding the print function for hash C solution

--- 500_Data_Structures_and_Algorithms_Interview/01.Find_a_pair_with_the_given_sum_in_an_array/pair_hash_table.c
+++ 500_Data_Structures_and_Algorithms_Interview/01.Find_a_pair_with_the_given_sum_in_an_array/pair_hash_table.c
@@ -27,22 +27,35 @@ typedef struct {
   int value;
 } HashItem;
 
-void findPair(int nums[], int n, int target) {
-  HashItem* hashTable = (HashItem*)calloc(n, sizeof(HashItem) * n);
+void findPair(HashItem* hash_table, int nums[], int n, int target) {
+  HashItem* map = (HashItem*)calloc(n, sizeof(HashItem) * n);
   for (int i = 0; i < n; i++) {
     int complement = target - nums[i];
-    if (hashTable[complement].key != 0) {
-      printf("Pair found (%d, %d)\n", hashTable[complement].key, nums[i]);
+    if (map[complement].key != 0) {
+      // printf("Pair found (%d, %d)\n", map[complement].key, nums[i]);
+      hash_table[i].key = map[complement].key;
+      hash_table[i].value = nums[i];
     }
-    hashTable[nums[i]].key = nums[i];
+    map[nums[i]].key = nums[i];
+  }
+}
+
+void printResult(HashItem* hashTable, int n) {
+  for (int i = 0; i < n; i++) {
+    if (hashTable[i].key != 0) {
+      printf("Pair found (%d, %d)\n", hashTable[i].key, hashTable[i].value);
+    }
   }
 }
 
 int main() {
   int nums[] = {8, 7, 2, 5, 3, 1}, target = 10;
   int n = sizeof(nums) / sizeof(nums[0]);
+  HashItem* hashTable = (HashItem*)calloc(n, sizeof(HashItem) * n);
 
-  findPair(nums, n, target);
+  findPair(hashTable, nums, n, target);
+
+  printResult(hashTable, n);
 
   return 0;
 }
